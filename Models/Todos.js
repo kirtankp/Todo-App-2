c19b43efc1e39f4817e09e020430bf63ce979fc5
kirtan <kirtan1902@gmail.com>
2023-08-02 18:24:39 +0530

    users based Todos are stored  in DB by userId

---
+++ Models/Todos.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+//todoSchema is the schema for the users
+const todoSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},
+    //userTodo is the collection name in the dataBase
+    { collection: 'userTodo' }
+)
+//Todo is the model name 
+module.exports = mongoose.model('Todo', todoSchema)
