d2bb090ff25a41e0b70ac6fff509b8dcbd8075f8
kirtan <kirtan1902@gmail.com>
2023-10-04 21:52:42 +0530

    UI work

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,33 +1,37 @@
+"use client"
+
 import React, { useState } from 'react';
+import Link from 'next/link';
 
 const Signup: React.FC = () => {
-//   const [formData, setFormData] = useState({
-//     username: '',
-//     email: '',
-//     password: '',
-//   });
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
 
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-//   const handleSubmit = (e: React.FormEvent) => {
-//     e.preventDefault();
-//     // Handle signup logic here (e.g., send data to the server)
-//   };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle signup logic here (e.g., send data to the server)
+    console.log(formData)
+  };
 
   return (
-    // <form onSubmit={handleSubmit}>
-    <form>
+    <form onSubmit={handleSubmit}>
+      {/* // <form> */}
       <h2 className="text-2xl mb-4">Sign Up</h2>
       <div className="mb-4">
         <input
           type="text"
           name="username"
           placeholder="Username"
-        //   onChange={handleChange}
-        //   value={formData.username}
+          onChange={handleChange}
+          value={formData.username}
           className="w-full px-3 py-2 border rounded"
         />
       </div>
@@ -36,8 +40,8 @@ const Signup: React.FC = () => {
           type="email"
           name="email"
           placeholder="Email"
-        //   onChange={handleChange}
-        //   value={formData.email}
+          onChange={handleChange}
+          value={formData.email}
           className="w-full px-3 py-2 border rounded"
         />
       </div>
@@ -46,17 +50,20 @@ const Signup: React.FC = () => {
           type="password"
           name="password"
           placeholder="Password"
-        //   onChange={handleChange}
-        //   value={formData.password}
+          onChange={handleChange}
+          value={formData.password}
           className="w-full px-3 py-2 border rounded"
         />
       </div>
       <button
         type="submit"
-        className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+        className="mb-4 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
       >
         Sign Up
       </button>
+      <Link href="/login" className="text-blue-600 rounded  hover:text-red-500 md:p-0 ">
+        Already Signed? Login
+      </Link>
     </form>
   );
 };
