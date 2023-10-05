d69a00a0a759615e3e3e5f59dd4904722d221d5b
kirtan <kirtan1902@gmail.com>
2023-10-05 16:20:47 +0530

    userData is stored in DB from client to server

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -21,7 +21,7 @@ const Login: React.FC = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ formData }),
+        body: JSON.stringify({ ...formData }),
       });
 
       if (response.ok) {
